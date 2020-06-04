fcb2c82b4eaf5ee7ef88f84ecf5e58ba21aed108
Michael Wiktorek <wiktorekmichael@gmail.com>
2020-06-03 22:51:59 -0700

    remove committed node_modules and add gitignore

--- src/components/Page.js
+++ src/components/Page.js
@@ -5,31 +5,47 @@ import { Style } from "react-style-tag";
 import { withStyles } from "@material-ui/core/styles";
 
 const styles = () => ({
-  topper: {
-    backgroundColor: "#eee",
-  },
-  root: {
-    maxWidth: "1168px",
-    marginTop: "94px",
-  },
+    topper: {
+        backgroundColor: "#eee",
+    },
+    root: {
+        maxWidth: "1168px",
+        marginTop: "94px",
+    },
 });
 
 const Component = ({ children, classes }) => {
-  return (
-    <div className={classes.topper}>
-      <Style>{`
+    return (
+        <div className={classes.topper}>
+            <Style>{`
       body {background: #eee}
     `}</Style>
-      <Grid alignItems="stretch" container justify="center" spacing={0}>
-        <Grid className={classes.root} item xs={12}>
-          <div style={{ padding: "16px" }}>
-            {children}
-            <Footer />
-          </div>
-        </Grid>
-      </Grid>
-    </div>
-  );
+            {/* <Grid alignItems="stretch" container justify="center" spacing={0}>
+        <Grid className={classes.root} item xs={12}> */}
+            <div
+                style={{
+                    display: "flex",
+                    justifyContent: "center",
+                    border: "1px solid blue",
+                }}
+            >
+                <div
+                    style={{
+                        maxWidth: "100%",
+                        marginTop: "94px",
+                        border: "1px solid red",
+                    }}
+                >
+                    <div style={{ padding: "16px" }}>
+                        {children}
+                        <Footer />
+                    </div>
+                </div>
+            </div>
+            {/* </Grid> */}
+            {/* </Grid> */}
+        </div>
+    );
 };
 
 export default withStyles(styles)(Component);
